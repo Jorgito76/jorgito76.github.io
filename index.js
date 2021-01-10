064a62bb5ed471d080eaeccf15e94b3bab6d1c6c
Jorgito76 <ova20003@byu.edu>
2021-01-09 22:52:22 -0700

    modified last updated js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function lastUpdated (){
-    //Setting the last modified date
-    let lastModified = new Date(document.lastModified);
-    //Display the date to user
-    document.getElementById("lastUpdated").innerHTML = lastModified;
+function lastModified(){
+    document.getElementById("lastUpdated").textContent = document.lastModified;
 }
