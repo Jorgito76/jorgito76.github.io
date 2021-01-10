52a85acf9b8c50963b404092f7d080fec2c53b91
Jorgito76 <ova20003@byu.edu>
2021-01-09 22:47:25 -0700

    updates to links

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function lastUpdated (){
     //Setting the last modified date
     let lastModified = new Date(document.lastModified);
     //Display the date to user
-    document.getElementById("lastUpdated").innerHTML = string(lastModified);
+    document.getElementById("lastUpdated").innerHTML = lastModified;
 }
