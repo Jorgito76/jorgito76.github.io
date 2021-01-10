c67a0c04b848ca84ee299cc89df6c1ba9b4577cb
Jorgito76 <ova20003@byu.edu>
2021-01-09 22:32:16 -0700

    last modified date

---
+++ index.js
@@ -0,0 +1,6 @@
+function lastUpdated (){
+    //Setting the last modified date
+    let lastModified = document.lastModified;
+
+    document.getElementById('lastupdated').innerHTML = lastModified;
+}
