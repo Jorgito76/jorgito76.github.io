ed21a1b729c95ce3359df04140b8360237c72f46
Jorgito76 <ova20003@byu.edu>
2021-04-07 08:11:06 -0600

    added apis to town forecast

--- lesson10/js/sodaSprings.js
+++ lesson10/js/sodaSprings.js
@@ -1,16 +1,15 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=f733a7a677f906c085116effb07477c2&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=f733a7a677f906c085116effb07477c2&units=imperial';
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject)
-        document.getElementById('name').textContent = jsObject.name;
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
-        document.getElementById('temp-max').textContent = jsObject.main.temp_max;
+        document.getElementById('temperature').textContent = jsObject.main.temp;
+        document.getElementById('hightemp').textContent = jsObject.main.temp_max;
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('speed').textContent = jsObject.wind.speed;
 
-        let temperature = parseFloat(document.getElementById('current-temp').innerHTML);
+        let temperature = parseFloat(document.getElementById('temperature').innerHTML);
         let speed = parseFloat(document.getElementById('speed').innerHTML);
 
         let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16))
@@ -26,7 +25,7 @@ fetch(apiURL)
     });
 
 
-const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=f733a7a677f906c085116effb07477c2&units=imperial';
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=560791&appid=f733a7a677f906c085116effb07477c2&units=imperial';
 fetch(forecastURL)
     .then((response) => response.json())
     .then((forecastObject) => {
@@ -47,13 +46,67 @@ fetch(forecastURL)
 
     });
 
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'
-fetch(requestURL)
+    const apiURL2 =
+  "////api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=300a54035f682f83ebf3aa7d44d398ba";
+
+const d = new Date();
+
+const todayDayNumber = d.getDay();
+
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+fetch(apiURL2)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    console.log(weatherInfo);
+
+
+    let mylist = weatherInfo.list;
+    let forecastDayNumber = todayDayNumber;
+
+    for (i = 0; i < mylist.length; i++) {
+      var time = mylist[i].dt_txt;
+      if (time.includes("18:00:00")) {
+        forecastDayNumber += 1;
+        if (forecastDayNumber === 7) {
+          forecastDayNumber = 0;
+        }
+
+        let theDayName = document.createElement("h4");
+        theDayName.textContent = weekday[forecastDayNumber];
+
+        let theTemp = document.createElement("p");
+        theTemp.innerHTML = weatherInfo.list[i].main.temp + "ºF";
+
+        let iconcode = weatherInfo.list[i].weather[0].icon;
+        let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+        let theIcon = document.createElement("img");
+        theIcon.src = iconPath;
+        theIcon.alt = `Icon image of ${weatherInfo.list[i].weather[0].description}`;
+
+        let theDay = document.createElement("div");
+        theDay.appendChild(theDayName);
+        theDay.appendChild(theIcon);
+        theDay.appendChild(theTemp);
+        document.getElementById("forecastDays").appendChild(theDay);
+      }
+    }
+  });
+
+  const requestURL2 = 'https://byui-cit230.github.io/weather/data/towndata.json'
+fetch(requestURL2)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
 
-        document.getElementById('event1').textContent = jsObject.towns[0].events[0];
-        document.getElementById('event2').textContent = jsObject.towns[0].events[1];
-        document.getElementById('event3').textContent = jsObject.towns[0].events[2];
+        document.getElementById('event1').textContent = jsObject.towns[6].events[0];
+        document.getElementById('event2').textContent = jsObject.towns[6].events[1];
+        document.getElementById('event3').textContent = jsObject.towns[6].events[2];
     });
